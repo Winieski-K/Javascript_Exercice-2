42874c85e9d93c2f7db9da3aca8c6601a99b1804
Winieski-K <k.winieski@hotmail.com>
2018-06-20 17:04:25 +0200

    Affiche contenu de plusieurs variables

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Déclaration des variables
+var lastName = 'Doe';
+var firstName = 'John';
+var city = 'New York';
+// Affichage des variables avec des concaténations
+alert ('Nom : ' + lastName + '\nPrénom : ' + firstName + '\nVille : ' + city);
